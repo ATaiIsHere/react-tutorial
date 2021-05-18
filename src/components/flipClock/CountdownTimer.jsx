ac8b765ac229a41ee3392b78b29a1af3bbc7fdcf
sean.tai <sean.tai@bionime.com>
2021-05-18 18:43:22 +0800

    建立倒數計時元件

---
+++ src/components/flipClock/CountdownTimer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CountdownTimer extends React.Component {
+    init = () => {
+        let { targetTime } = this.props;
+        
+        
+
+    }
+
+    render() {
+
+    }
+}
+
+export default CountdownTimer;
