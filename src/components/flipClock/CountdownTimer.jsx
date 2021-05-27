28bda94b45fa69115588c36f8cd033d405e9404c
Sean Tai <ataiishere34@gmail.com>
2021-05-27 13:11:52 +0800

    實作倒數計時元件細節

--- src/components/flipClock/CountdownTimer.jsx
+++ src/components/flipClock/CountdownTimer.jsx
@@ -1,16 +1,87 @@
+import { TransferWithinAStationRounded } from '@material-ui/icons';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
+import Flipper from './Flipper';
+import './countdownTimer.css';
 
 class CountdownTimer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.timer = null;
+
+        this.flippers = [
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+        ];
+    }
+
+    componentDidMount = () => {
+        this.init();
+        this.run();
+    };
+
     init = () => {
+        let leftTimeStr = this.getLeftTime();
+        console.log(this.flippers);
+        this.flippers.forEach((ref, i) => {
+            ref.current.setFront(leftTimeStr.charAt(i))
+        });
+    };
+
+    run = () => {
+        this.time = setInterval(() => {
+            let leftTimeStr = this.getLeftTime();
+            this.flippers.forEach((ref, i) => {
+                let currentDigital = ref.current.state.frontTextFromData;
+                let nextDigital = leftTimeStr.charAt(i);
+
+                if (currentDigital != nextDigital) {
+                    ref.current.flipDown(currentDigital, nextDigital);
+                }
+            });
+        }, 1000);
+    };
+
+    getLeftTime = () => {
         let { targetTime } = this.props;
-        
-        
+        let secDiff = ~~((targetTime - new Date()) / 1000);
 
-    }
+        let displaySec = secDiff % 60;
+        let displayMin = ~~((secDiff % 3600) / 60);
+        let displayHour = ~~((secDiff % 86400) / 3600);
+        let displayDay = ~~(secDiff / 86400);
 
-    render() {
+        return `${this.parseIntToStr(displayDay)}${this.parseIntToStr(displayHour)}${this.parseIntToStr(displayMin)}${this.parseIntToStr(displaySec)}`;
+    };
 
-    }
+    parseIntToStr = (intVal) => intVal.toString().padStart(2, '0');
+
+    render = () => {
+        return (
+            <div className="countdown-timer">
+                <em>剩餘</em>
+                <Flipper ref={this.flippers[0]} />
+                <Flipper ref={this.flippers[1]} />
+                <em>天</em>
+                <Flipper ref={this.flippers[2]} />
+                <Flipper ref={this.flippers[3]} />
+                <em>小時</em>
+                <Flipper ref={this.flippers[4]} />
+                <Flipper ref={this.flippers[5]} />
+                <em>分</em>
+                <Flipper ref={this.flippers[6]} />
+                <Flipper ref={this.flippers[7]} />
+                <em>秒</em>
+            </div>
+        );
+    };
 }
 
 export default CountdownTimer;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import BasicTimeline from './timelineDemo';
 import Game from './components/ticTacToe/Game';
+import CountdownTime from './components/flipClock/CountdownTimer';
+import FlipClock from './components/flipClock/FlipClock';
 
 // ========================================
 
 // ReactDOM.render(<Game />, document.getElementById('root'));
-ReactDOM.render(BasicTimeline(), document.getElementById('root'));
+// ReactDOM.render(BasicTimeline(), document.getElementById('root'));
+ReactDOM.render(<CountdownTime targetTime={new Date('2021-05-31T10:20:30Z')} />, document.getElementById('root'));
+// ReactDOM.render(<FlipClock />, document.getElementById('root'));
